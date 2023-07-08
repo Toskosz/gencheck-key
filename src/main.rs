059a5a73ad38d7493cd199bc057f89431db0d109
TokarskiThiago <thiago.tokarski2001@gmail.com>
2023-07-08 15:46:01 -0300

    new main_utils module

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 pub mod aes_gcm;
 pub mod rsa;
+pub mod main_utils;
+
+use rsa::bigint::BigInt;
+use rsa::generate_keypair;
+use rsa::rsa_encrypt;
+use rsa::rsa_decrypt;
+use main_utils::transform_u64_to_array_of_u8;
+use main_utils::reverse;
 
 fn main() {
     println!("Hello, world!");
