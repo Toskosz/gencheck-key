cede6a60b26ecc86c53dfc78364f4fd84e58728e
TokarskiThiago <thiago.tokarski2001@gmail.com>
2023-07-10 00:17:26 -0300

    change paths

--- src/main.rs
+++ src/main.rs
@@ -30,27 +30,27 @@ fn main() {
 
 fn generate_aes_key(){
     let key = generate_128bit_key();
-    fs::write("/home/thiago/gencheck-key/results/aes_key.txt", key).expect("Unable to write file");
+    fs::write("./results/aes_key.txt", key).expect("Unable to write file");
 }
 
 fn rsa_oaep_decode() {
-    let encoded_message = std::fs::read("/home/thiago/gencheck-key/results/rsa_encoded_message.txt").expect("Unable to read file");
-    let auth_data = std::fs::read("/home/thiago/gencheck-key/results/rsa_auth_data.txt").expect("Unable to read file");
-    let key_in_bytes = std::fs::read("/home/thiago/gencheck-key/results/rsa_private_key.txt").expect("Unable to read file");
+    let encoded_message = std::fs::read("./results/rsa_encoded_message.txt").expect("Unable to read file");
+    let auth_data = std::fs::read("./results/rsa_auth_data.txt").expect("Unable to read file");
+    let key_in_bytes = std::fs::read("./results/rsa_private_key.txt").expect("Unable to read file");
 
     let data = rsa::rsa_oaep_decode(encoded_message, auth_data, key_in_bytes);
 
-    fs::write("/home/thiago/gencheck-key/results/rsa_decoded_message.txt", data).expect("Unable to write file");
+    fs::write("./results/rsa_decoded_message.txt", data).expect("Unable to write file");
 }
 
 fn rsa_oaep_encode() {
-    let message = std::fs::read("/home/thiago/gencheck-key/results/aes_key.txt").expect("Unable to read file");
-    let auth_data = std::fs::read("/home/thiago/gencheck-key/results/rsa_auth_data.txt").expect("Unable to read file");
-    let key_in_bytes = std::fs::read("/home/thiago/gencheck-key/results/rsa_public_key.txt").expect("Unable to read file");
+    let message = std::fs::read("./results/aes_key.txt").expect("Unable to read file");
+    let auth_data = std::fs::read("./results/rsa_auth_data.txt").expect("Unable to read file");
+    let key_in_bytes = std::fs::read("./results/rsa_public_key.txt").expect("Unable to read file");
 
     let data = rsa::rsa_oaep_encode(message, auth_data, key_in_bytes);
 
-    fs::write("/home/thiago/gencheck-key/results/rsa_encoded_message.txt", data).expect("Unable to write file");
+    fs::write("./results/rsa_encoded_message.txt", data).expect("Unable to write file");
 }
 
 fn generate_rsa_keys() {
@@ -61,30 +61,30 @@ fn generate_rsa_keys() {
     let public_key_in_right_order = main_utils::byte_reverse(&public_key);
     let private_key_in_right_order = main_utils::byte_reverse(&private_key);
 
-    fs::write("/home/thiago/gencheck-key/results/rsa_public_key.txt", public_key_in_right_order).expect("Unable to write file");
-    fs::write("/home/thiago/gencheck-key/results/rsa_private_key.txt", private_key_in_right_order).expect("Unable to write file");
+    fs::write("./results/rsa_public_key.txt", public_key_in_right_order).expect("Unable to write file");
+    fs::write("./results/rsa_private_key.txt", private_key_in_right_order).expect("Unable to write file");
 }
 
 fn aes_encode() {
-    let key = std::fs::read("/home/thiago/gencheck-key/results/aes_key.txt").expect("Unable to read file");
+    let key = std::fs::read("./results/aes_key.txt").expect("Unable to read file");
     if key.len() != 16 {
         panic!("Key must be 16 bytes long");
     }
     
-    let mut input = std::fs::read("/home/thiago/gencheck-key/results/aes_message.txt").expect("Unable to read file");
+    let mut input = std::fs::read("./results/aes_message.txt").expect("Unable to read file");
     let output = core_encrypt(&mut input, &key);
 
-    fs::write("/home/thiago/gencheck-key/results/aes_encoded_message.txt", output).expect("Unable to write file");
+    fs::write("./results/aes_encoded_message.txt", output).expect("Unable to write file");
 }
 
 fn aes_decode() {
-    let key = std::fs::read("/home/thiago/gencheck-key/results/aes_key.txt").expect("Unable to read file");
+    let key = std::fs::read("./results/aes_key.txt").expect("Unable to read file");
     if key.len() != 16 {
         panic!("Key must be 16 bytes long");
     }
     
-    let mut input = std::fs::read("/home/thiago/gencheck-key/results/aes_encoded_message.txt").expect("Unable to read file");   
+    let mut input = std::fs::read("./results/aes_encoded_message.txt").expect("Unable to read file");   
     let output = core_decrypt(&mut input, &key);
 
-    fs::write("/home/thiago/gencheck-key/results/aes_decoded_message.txt", output).expect("Unable to write file");
+    fs::write("./results/aes_decoded_message.txt", output).expect("Unable to write file");
 }
